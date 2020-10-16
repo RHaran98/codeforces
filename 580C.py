68733babfb8418eff414f44b586b91bbefdba405
RHaran98 <rharan1998@gmail.com>
2020-10-16 21:47:30 +0530

    Added soln for 580C

--- 580C.py
+++ 580C.py
@@ -1,52 +1,51 @@
 class Node:
   def __init__(self,value,parent=None):
-    self.childrens = []
+    self.neighbors = set()
     self.value = value
     self.cat = 0
     self.parent = parent
     self.isVisited = False
 
-  def add_child(self,child):
-    # child = Node(child,parent=self.value)
-    self.childrens.append(child)
+  def add_neighbor(self,node):
+    self.neighbors.add(node)
 
-  def set_parent(self, parent):
-    self.parent = parent
-  
   def __repr__(self):
-    return f"{self.value} has {self.cat} cats, {len(self.childrens)} children"
+    return f"Node{self.value}"
+
 
 class Graph:
-  def __init__(self):
-    self.nodes = {}
-    self.root = Node(1)
-    self.nodes[1] = self.root
+  def __init__(self,n):
+    self.nodes = { x:Node(x) for x in range(1,n+1) }
+    self.isVisited = {i:False for i in range(1,n+1)}
+    self.root = self.nodes[1]
+    
+  def get_node(self,value):
+    if value in self.nodes:
+      return self.nodes[value]
+    else:
+      return Node(value)
 
   def add_edge(self,src,dst):
-    src,dst = sorted((src,dst))
-    if not src in self.nodes:
-      self.nodes[src] = Node(src)
-    src = self.nodes[src]
+    src = self.get_node(src)
+    dst = self.get_node(dst)
 
-    if not dst in self.nodes:
-      self.nodes[dst] = Node(dst)
-    dst = self.nodes[dst]
-
-    src.add_child(dst)
-    dst.set_parent(src)
-
-    dst.add_child(src)
-    src.set_parent(dst)
-
-
+    src.add_neighbor(dst)
+    dst.add_neighbor(src)
 
   def hasCat(self,node):
     self.nodes[node].cat = 1
+
+  def visit_node(self,node):
+    self.isVisited[node.value] = True
+
+  def get_neighbors(self,node):
+    return [i for i in node.neighbors if not self.isVisited[i.value]]  
+
     
 def make_graph():
   v, limit = (map(int,input().split()))
   cats = list(map(int,input().split()))
-  graph = Graph()
+  graph = Graph(v)
   for i in range(v-1):
     src,dst = list(map(int,input().split()))
     graph.add_edge(src,dst)
@@ -56,29 +55,27 @@ def make_graph():
   return graph, limit
 
 def custom_dfs(graph, limit):
-  # catstreak = graph.root.cat
-  def inner(node, catstreak, limit):
-    print("Parent",node.value, catstreak)
-    if (catstreak + node.cat) > limit:
-      return 0
-    if len(node.childrens) == 0:
-      print("Path!",node, catstreak)
-      return 1
+  stack = [(graph.root,0)]
+  restaurants = 0
+  while stack:
+    node, catstreak = stack.pop()
+    graph.visit_node(node)
+    if ((catstreak + node.cat) > limit):
+      continue
 
-    catstreak = catstreak+1 if node.cat else 0
-    rests = 0
-    node.isVisited = True
-    for child in node.childrens:
-      if not child.isVisited:
-        print("\t{}-Child".format(node.value),child.value, catstreak)
-        rests += inner(child, catstreak, limit)
-    return rests
-
-  return inner(graph.root, 0, limit)
+    if not node.cat :
+      catstreak = 0
+    else:
+      catstreak +=1
+    neighbors = graph.get_neighbors(node) 
+    if len(neighbors) == 0:
+      restaurants +=1
+    else:
+      stack.extend([(i,catstreak) for i in neighbors])   
+  return restaurants
   
 
 if __name__ == "__main__":
   graph, limit = make_graph()
-  print(graph.nodes)
   print(custom_dfs(graph, limit))
   
