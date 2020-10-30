4dc61c973643838ebffa56dfb0d50a321e84bf4d
RHaran98 <rharan1998@gmail.com>
2020-10-30 23:35:07 +0530

    Added efficient soln to 431C with memoization

--- 431C.py
+++ 431C.py
@@ -1,22 +1,22 @@
 from functools import lru_cache
 
-@lru_cache(200)
+# arr = [1 for i in range(10)]
 def fsum(n,k):
   if k > n:
     k = n
   if n == 0:
-    return 0
-  paths = k
+    return 1
+  # paths = 1
+  paths = 0
+  # print(n,k, paths)
   for i in range(1,k+1):
-    paths += fsum(n-i,k)
+    paths += (fsum(n-i,k))
   return paths
 
-  
-# for n in range(1,4):
-#   for k in range(1,4):
-#     for d in range(1,k+1):
-#       a = fsum(n,k)
-#       b = fsum (n,d-1)
-#       print(n,k,d ,f"{a}-{b}={a-b}")
-
-fsum(3,3)
+# print(fsum(3,3))
+# print(fsum(3,0))
+n,k,d = map(int,input().split(" "))
+# a = fsum(n,k)
+# b = fsum (n,d-1)
+# print(n,k,d ,f"{a}-{b}={a-b}")
+# print((fsum(n,k) - fsum(n,d-1))% 1000000007)
