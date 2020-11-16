eb5cad55b8189f81368fbe4c080ceca614be5a79
RHaran98 <rharan1998@gmail.com>
2020-11-16 21:58:19 +0530

    Correct soln to 455A (DP)

--- 455A.py
+++ 455A.py
@@ -1,25 +1,29 @@
+def test():
+  n = int(input())
+  seq = list(map(int, input().split()))
 
-n = int(input())
+  counts = {}
+  for i in seq:
+    counts[i] = counts.get(i,0) + 1
+  keys = sorted(list(counts.keys()))
+  # if len(keys) == 1:
+  #   print(keys[0]*counts[keys[0]])
+  #   return
+  tab = [0 for i in range(max(keys)+1)]
+  # tab[0] = counts.get(0,0)*0
+  # tab[1] = counts.get(1,0)*1
+  # tab[1] += tab[0] if abs(keys[0] - keys[1]) != 1 else 0
+  # if len(keys) == 2:
+  #   print(max(*tab))
+  #   return
+  # for i in range(2,len(tab)):
+  #   print(i,keys[i])
+  for i in range(1,len(tab)):
+    # if abs(keys[i] - keys[i-1]) != 1:
+    #   tab[i] = counts[keys[i]] * keys[i] + tab[i-1]
+    # else:
+    tab[i] = max(counts.get(i,0)*(i) + tab[i-2], tab[i-1])
 
-tab = [[0 for i in range(n)]]
+  print(tab[-1])
 
-
-def score(seq, currentScore):
-  maxScore = currentScore
-  print(seq)
-  if not seq:
-    return maxScore
-  if len(seq) == 1:
-    maxScore += seq[0]
-    return maxScore 
-  seq_set = set(seq)
-  maxScorelist = []
-  for i in seq_set:
-    if i not in seq:
-      continue
-    sub_list = [j for j in seq if ((j != (i-1)) and (j != (i+1)))]
-    sub_list.pop(sub_list.index(i))
-    maxScore += i + score(sub_list, currentScore)
-    maxScorelist.append(maxScore)
-  return max(maxScorelist)
-
+test()
